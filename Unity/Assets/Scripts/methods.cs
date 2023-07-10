14f9cd43cd2cfe4dbe2e70a7b0c4c8f177168d30
vistasla <vistasla@163.com>
2023-07-10 22:43:36 +0800

    /3ds Max/

---
+++ Unity/Assets/Scripts/methods.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class methods : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HumanDescription.human human1 = GetComponent<human>();
+       
+
+    }
+}
+
+/*void Update()
+{
+
+   
+    position.x = position.x + 30.0f * horizontal * Time.deltaTime;
+    position.y = position.y + 30.0f * vertical * Time.deltaTime;
+    
+}*/
+/*public void ButtonOnClick()
+{
+   
+
+}*/
+/*position.y = position.y + (double)(0.1f* vertical);
+position.x = position.x + (double)(0.1f * horizontal);*/
+
+/*HingeJoint hinge = gameObject.GetComponent(typeof(HingeJoint)) as HingeJoint;
+  TextMesh texture = (TextMesh)FindObjectOfType(typeof(TextMesh));*/
+/*go1 = GameObject.Find("RightHand");*/
+/*if (Input.GetKeyDown(KeyCode.P))
+{
+    print("P");
+    GetComponent<ParticleSystem>().Play();
+}*/
+/*new Vector3(90F, 0F, 0F);*/
+/*go2 = GameObject.FindWithTag("New tag");*/
+/*Destroy(FindObjectOfType<TextMesh>());*/
+/*if (Input.GetKeyDown(KeyCode.P))
+        {
+            print("P");
+            go2.transform.position = go1.transform.position;
+            go2.transform.rotation = new Quaternion(0F,0F,90F,0F);
+
+        }*/
+/*go = this.gameObject;*/
+/*go.SetActive(false);*/
+/*void ApplyDamage(float damage)
+{
+    print(damage);
+}*/
+/*component.GetComponentInParent(typeof(HingeJoint));*/
+/*hinge = GetComponentInParent<HingeJoint>(true);*/
+/*DeltaPositionArr.Length =; 
+ * (Assets\MyCarController.cs(19, 9): error CS0200: Property or indexer 'Array.Length' cannot be assigned to -- it is read only
+
+)*/
+/*Object[] myObjArray = new Object[5] { 26, 27, 28, 29, 30 };*/
